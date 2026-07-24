36f0019ed23e9cc20354b2c2b713b3e9611e6558
Adam Bourn <adam.john.bourn@gmail.com>
2026-07-24 16:28:00 -0700

    barebones keycloak manifests

--- generate.sh
+++ generate.sh
@@ -6,6 +6,9 @@ helm template -n argocd argocd ./charts/argo-cd --values ./values/argo-cd/values
 echo "Generating Prometheus manifests..."
 helm template -n prometheus prometheus ./charts/prometheus --values ./values/prometheus/values.yaml > ./manifests/prometheus/manifest.yaml
 
+echo "Generating Keycloak manifests..."
+helm template -n keycloak keycloak ./charts/keycloakx --values ./values/keycloak/values.yaml > ./manifests/keycloak/manifest.yaml
+
 echo "Generating Guestbook manifests..."
 helm template -n guestbook-staging guestbook-staging ./charts/helm-guestbook --values ./values/guestbook/staging/values.yaml > ./manifests/guestbook/staging/manifest.yaml
 helm template -n guestbook-prod guestbook-prod ./charts/helm-guestbook --values ./values/guestbook/prod/values.yaml > ./manifests/guestbook/prod/manifest.yaml
